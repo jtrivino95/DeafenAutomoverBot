253203bee3436274a3cff562f2f7441c2bf2a3be
Javier Triviño <kal3it@gmail.com>
2021-03-15 18:48:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ bot.on('ready', () => {
 })
 
 prev_channel = [];
-const canal_AFK_id = '785554617080873000'
-const canal_Imperio_de_Diego = '411660383577702402'
+const channel_AFK_id = '785554617080873000'
+const channel_ACTUAL_id = '411660383577702402'
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
 	// When somebody selfdeafs
 	if (!oldMember.selfDeaf && newMember.selfDeaf) {
 		prev_channel[newMember.member.id] = newMember.channelID
-		newMember.setChannel(canal_AFK_id)
+		newMember.setChannel(channel_AFK_id)
 	}
 
 	// When somebody selfundeafs
 	if (oldMember.selfDeaf && !newMember.selfDeaf) {
-		if(newMember.channelID === canal_AFK_id){
+		if(newMember.channelID === channel_AFK_id){
 			if (prev_channel[newMember.member.id]) {
 				newMember.setChannel(prev_channel[newMember.member.id])
 			} else {
-				newMember.setChannel(canal_Imperio_de_Diego)
+				newMember.setChannel(channel_ACTUAL_id)
 			}
 		}
 	}
